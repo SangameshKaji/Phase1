e45de98367b0bbd572fa9ddcbba5fca50ea19ac9
Sangamesh <sangameshkaji10@gmail.com>
2026-01-26 23:51:10 +0530

    Not done

---
+++ 1. Recursion Fundamentals/Q9.java
@@ -0,0 +1,23 @@
+//9. Check whether 2 strings are same or not using recursion
+import java.util.*;
+
+public class Q9{
+    static boolean same(String s1, String s2,int i){
+        if(s1.length()!= s2.length()){
+            return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        try(Scanner sc = new Scanner(System.in)){
+            String s1 = sc.next();
+            String s2 = sc.next();
+            if(same(s1, s2,0)){
+                System.out.println("Both words are same");
+            }else{
+                System.out.println("Not same");
+            }
+        }
+    }
+}
