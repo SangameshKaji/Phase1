1cb1f90d46b70a880659266f8fb5ba350b7837b7
Sangamesh <sangameshkaji10@gmail.com>
2026-02-06 21:51:05 +0530

    11. Check whether one string is a subsequence of the other or not using recursion

--- 1. Recursion Fundamentals/Q11.java
+++ 1. Recursion Fundamentals/Q11.java
@@ -1,5 +1,23 @@
 //11. Check whether one string is a subsequence of the other or not using recursion
 import java.util.*;
 public class Q11 {
-    
+    static boolean sub(String s1,String s2,int a,int b){
+        if(s2.length() == b) return true;
+        if(s1.length() == a) return false;
+        if(s1.charAt(a)==s2.charAt(b)){
+            return sub(s1,s2,a+1,b+1);
+        }
+        return sub(s1,s2,a+1,b);
+    }
+    public static void main(String[] args) {
+        try(Scanner sc = new Scanner(System.in)){
+            String s1 = sc.next();
+            String s2 = sc.next();
+            if(sub(s1,s2,0,0)){
+                System.out.println("Yes");
+            }else{
+                System.out.println("No");
+            }
+        }
+    }
 }
