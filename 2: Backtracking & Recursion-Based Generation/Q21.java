15bb39d991979496d6cac63a8d616ac285119cb2
Sangamesh <sangameshkaji10@gmail.com>
2026-02-14 13:05:45 +0530

    21. Generate Parentheses

---
+++ 2: Backtracking & Recursion-Based Generation/Q21.java
@@ -0,0 +1,9 @@
+//21. Generate Parentheses
+import java.util.*;
+public class Q21 {
+    public static void main(String[] args) {
+        try(Scanner sc = new Scanner(System.in)){
+            
+        }
+    }
+}
