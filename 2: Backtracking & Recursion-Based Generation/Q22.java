56673a0ed87484c5d3ef7a6bfd5f688e2c136f84
Sangamesh <sangameshkaji10@gmail.com>
2026-02-17 22:03:47 +0530

    22. Generate all Binary Strings

--- 2: Backtracking & Recursion-Based Generation/Q22.java
+++ 2: Backtracking & Recursion-Based Generation/Q22.java
@@ -1,7 +1,15 @@
 //22. Generate all Binary Strings
+import java.util.*;
 
-public class Q22 {
+public class Q22{
+    static ArrayList<String> binstr(int n) {
+        ArrayList<String> ar = new ArrayList<>();
+        return ar;
+    }
     public static void main(String[] args) {
-        
+        try(Scanner sc = new Scanner(System.in)){
+            int n = sc.nextInt();
+            System.out.println(binstr(n));
+        }
     }
 }
