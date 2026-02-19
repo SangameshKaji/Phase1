fd5f19561d731d3e807070f7329aed5bad04b948
Sangamesh <sangameshkaji10@gmail.com>
2026-02-19 20:46:26 +0530

    .

--- 2: Backtracking & Recursion-Based Generation/Q22.java
+++ 2: Backtracking & Recursion-Based Generation/Q22.java
@@ -2,14 +2,24 @@
 import java.util.*;
 
 public class Q22{
-    static ArrayList<String> binstr(int n) {
-        ArrayList<String> ar = new ArrayList<>();
-        return ar;
+    public static ArrayList<String> binstr(int n) {
+        ArrayList<String> res = new ArrayList<>();
+        backtrack(n, "", res);
+        return res;
+    }
+
+    public static void backtrack(int n, String curr, ArrayList<String> res) {
+        if (curr.length() == n) {
+            res.add(curr);
+            return;
+        }
+        backtrack(n, curr + "0", res);
+        backtrack(n, curr + "1", res);
     }
     public static void main(String[] args) {
         try(Scanner sc = new Scanner(System.in)){
-            int n = sc.nextInt();
-            System.out.println(binstr(n));
+            // int n = sc.nextInt();
+            System.out.println(binstr(2));
         }
     }
 }
