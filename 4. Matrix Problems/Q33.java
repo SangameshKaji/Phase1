979bae6e9c4c94d76b6238cee1e52f396250471c
Sangamesh <sangameshkaji10@gmail.com>
2026-03-06 19:58:23 +0530

    //33. Rotate by 90 Degrees

--- 4. Matrix Problems/Q33.java
+++ 4. Matrix Problems/Q33.java
@@ -3,18 +3,33 @@ import java.util.*;
 
 public class Q33 {
     static void rotate(int mat[][]) {
-        
+        int n = mat.length;
+        int arr[][] = new int[n][n];
+        for(int i = 0; i < n; i++){
+            arr[i] = mat[i].clone();
+        }
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<n;j++){
+                mat[i][j] = arr[n-1-j][i];
+            }
+        }
     }
     public static void main(String[] args) {
         try(Scanner sc = new Scanner(System.in)){
             int n = sc.nextInt();
             int arr[][] = new int[n][n];
             for(int i = 0;i<n;i++){
-                for(int j = 0;j<n;i++){
+                for(int j = 0;j<n;j++){
                     arr[i][j] = sc.nextInt();
                 }
             }
             rotate(arr);
+            for(int i = 0;i<n;i++){
+                for(int j = 0;j<n;j++){
+                    System.out.print(arr[i][j]+" ");
+                }
+                System.out.println();
+            }
         }
     }    
 }
