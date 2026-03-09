7db1efd81770e5cf4d542e8ac2e40b896efa9e16
Sangamesh <sangameshkaji10@gmail.com>
2026-03-09 22:14:15 +0530

    36. Valid Anagram

--- 5. Hashing & Strings/Q36.java
+++ 5. Hashing & Strings/Q36.java
@@ -1,7 +1,32 @@
 //36. Valid Anagram
+import java.util.*;
 
 public class Q36 {
+    static boolean anagram(String s, String t){
+        if(s.length() != t.length()) return false;
+        int count[] = new int[26];
+        for(final char c : s.toCharArray()){
+            ++count[c-'a'];
+        }
+
+        for(final char c : t.toCharArray()){
+            if(count[c-'a']==0){
+                return false;
+            }else{
+                --count[c-'a'];
+            }
+        }
+        return true;
+    }
     public static void main(String[] args) {
-        
+        try(Scanner sc = new Scanner(System.in)){
+            String s = sc.nextLine();
+            String t = sc.nextLine();
+            if(anagram(s,t)){
+                System.out.println("They are valid anagram");
+            }else{
+                System.out.println("Not a valid anagram");
+            }
+        }
     }
 }
