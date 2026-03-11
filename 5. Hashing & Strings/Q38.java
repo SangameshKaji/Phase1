39d58bfde5cae8528160ec1b410210b41ee20bdf
Sangamesh <sangameshkaji10@gmail.com>
2026-03-11 21:18:16 +0530

    .

---
+++ 5. Hashing & Strings/Q38.java
@@ -0,0 +1,13 @@
+//38. Simplify Path
+import java.util.*;
+
+public class Q38 {
+    public String simplifyPath(String path) {
+        
+    }
+    public static void main(String[] args) {
+        try(Scanner sc = new Scanner(System.in)){
+            String path = sc.nextLine();
+        }
+    }
+}
