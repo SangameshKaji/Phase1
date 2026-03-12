57bcbf63be5fc0f76e02674209ebe4b2373cf759
Sangamesh <sangameshkaji10@gmail.com>
2026-03-12 12:44:54 +0530

    38. Simplify Path

--- 5. Hashing & Strings/Q38.java
+++ 5. Hashing & Strings/Q38.java
@@ -2,12 +2,19 @@
 import java.util.*;
 
 public class Q38 {
-    public String simplifyPath(String path) {
-        
+    static String simplifyPath(String path) {
+        Stack<String> st = new Stack<>();
+        for(String s : path.split("/")){
+            if(s.equals("..")){
+                if(!st.isEmpty()) st.pop();
+            }else if(s.length()>0 && !s.equals(".")) st.push(s);
+        }
+        return "/"+String.join("/",st);
     }
     public static void main(String[] args) {
         try(Scanner sc = new Scanner(System.in)){
             String path = sc.nextLine();
+            System.out.println(simplifyPath(path));
         }
     }
 }
